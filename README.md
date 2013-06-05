2931801d72f548753dec4e20701b2da58b0be70c
DolphinBoy <dolphinboyo@gmail.com>
2013-06-05 18:13:11 +0800

    页面:修改首页;基本做出便签列表页面

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ CloudMemo主要有以下功能：
 2. 支持便签模版选择
 3. 支持移动客户端
 
+便签制作:
+http://www.cnblogs.com/TomXu/archive/2011/12/13/2285755.html
+
 前端插件：
 
 消息提示插件：  
@@ -27,11 +30,11 @@ https://github.com/carhartl/jquery-cookie
 便签样式参考:  
 http://www.w3cplus.com/solution/css3box/css3box.html
 
-设计风格：
+设计风格：  
 http://designmodo.github.io/Flat-UI/
 
-参考网站:
-http://www.lofter.com/
+参考网站:  
+http://www.lofter.com/  
 http://wujb.cnodejs.net/
 
 参考架构：  
@@ -41,5 +44,9 @@ http://cnodejs.org/topic/5007bd684764b72902c6aec9
 http://www.qiqiboy.com/2010/11/27/javascript-get-bing-image.html
 http://www.w3c.com.cn/nodejs%E4%B8%8B%E8%BD%BD%E5%9B%BE%E7%89%87%E6%97%B6%E9%81%87%E5%88%B0%E9%98%B2%E5%A4%96%E9%93%BE%E7%9A%84%E5%9B%BE%E7%89%87%E6%97%A0%E6%B3%95%E4%B8%8B%E8%BD%BD
 
-资源网址：
+资源网址：  
 http://www.daimg.com/search.php?kwtype=0&keyword=%B1%E3%C7%A9&channeltype=0&ext=0&pdi=0&size=0&starttime=-1&free=0
+
+
+建议升级浏览器:  
+http://onlinefontconverter.com/font-formats
---
+++ server/bing.js
@@ -0,0 +1,48 @@
+
+function getBingBackground(url, ) {  //获取bing每日首页背景图片
+	url = url || "http://bing.com";
+	var html = '';
+	// 1.取得首页的html内容
+	var req = http.get(url, function(res) {
+		  console.log("Got response: " + res.statusCode);
+		  res.on('data', function (chunk) {
+			    console.log('BODY: ' + chunk);
+			    html = chunk;
+		  });
+		})
+	req.on('error', function(e) {
+		console.log("Download bing background failed!");
+	    console.log("Got error: " + e.message);
+	});
+	
+	var imgurl = '';
+	
+}
+//#bgDiv{opacity:1;background-image:url(http://s.cn.bing.net/az/hprichbg/rb/PatchworkLandscape_ZH-CN6728662377_1366x768.jpg);}
+/**
+	var download_file_httpget = function(file_url) {
+		var options = {
+		    host: url.parse(file_url).host,
+		    port: 80,
+		    path: url.parse(file_url).pathname
+
+		};
+
+		//use the date as the file name
+		var date = new Date();
+		var file_name = (date.getMonth()+1).toString() + '-' + date.getDate().toString();
+		file_name += '.jpg';
+		
+		var file = fs.createWriteStream(download_path + "\\" + file_name);
+
+		http.get(options,function(res) {
+		    res.on('data',function(data) {
+		        file.write(data);
+		    }).on('end',function() {
+		        file.end();
+		        console.log('download success');
+		    });
+		});
+	
+	
+}*/ //参考(http://www.w3c.com.cn/nodejs%E4%B8%8B%E8%BD%BD%E5%9B%BE%E7%89%87%E6%97%B6%E9%81%87%E5%88%B0%E9%98%B2%E5%A4%96%E9%93%BE%E7%9A%84%E5%9B%BE%E7%89%87%E6%97%A0%E6%B3%95%E4%B8%8B%E8%BD%BD) 
